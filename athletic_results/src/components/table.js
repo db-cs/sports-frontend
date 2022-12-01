ab81b9ef141f3ab8e403287ef327ac210f60da47
Nathan Bailey <dbailey@k12k.com>
2022-12-01 18:05:29 +0000

    Update table with headings for actual table & sty

--- athletic_results/src/components/table.js
+++ athletic_results/src/components/table.js
@@ -1,47 +1,44 @@
-const TableRow = ({first, last, handle}) => (
-  <tr className="bg-white border-b">
-    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-      {first}
+const TableRow = ({date, sport, results}) => (
+  
+  <tr className="even:bg-[#E3E3E3] odd:bg-[#575252] even:text-black odd:text-[#E3E3E3] border-b">
+    <td className="font-light px-6 py-4 whitespace-nowrap">
+      {date}
     </td>
-    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-      {last}
+    <td className="font-light px-6 py-4 whitespace-nowrap">
+      {sport}
     </td>
-    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-      {handle}
+    <td className="font-light px-6 py-4 whitespace-nowrap">
+      {results}
     </td>
   </tr>
 )
 
 export const Table = ({data}) => (
   <div className="flex flex-col">
-  <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
-      <div className="overflow-hidden">
-        <table className="min-w-full text-center">
-          <thead className="border-b bg-gray-50">
-            <tr>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                #
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                First
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                Last
-              </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
-                Handle
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map(user => <TableRow first={user.first} last={user.last} handle={user.handle} />)}
-            <TableRow first="Bladen" last="Hooper" handle="@bladen005"/>
-          </tbody>
-        </table>
+    <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+        <div className="overflow-hidden">
+          <table className="min-w-full text-center">
+            <thead className="border-b bg-[#E3E3E3]">
+              <tr>
+                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                  Date
+                </th>
+                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                  Sport
+                </th>
+                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4">
+                  Results
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(game_entry => <TableRow date={game_entry.date} sport={game_entry.sport} results={game_entry.results} />)}
+              <TableRow date="Manual Addition" sport="Manual Addition" results="Manual Addition"/>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
-</div>
 )
