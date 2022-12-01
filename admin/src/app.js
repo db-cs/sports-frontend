6a48d0c64801d40ae02a865a4cabf2995f4e421b
Adyn Smith <adynasmith@students.k12k.com>
2022-12-01 18:02:43 +0000

    Moved everything around. Added conditonal render.

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,3 +1,11 @@
-export function App() {
-    return <h1>Hello world!</h1>;
-  }
+import { Login } from "./components/login";
+import { Dashboard } from "./components/dashboard";
+//export function App() {
+//    return <h1>Hello world!</h1>;
+//  }
+
+export function App(props) {
+  const isLoggedIn = false;
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return isLoggedIn ? <Dashboard /> : <Login />;
+}
