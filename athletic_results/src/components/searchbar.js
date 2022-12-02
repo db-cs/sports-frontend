7986066f754116373fed9e7c106bbbbabca01fdd
Mei Hong <meilhong@students.k12k.com>
2022-12-02 17:31:02 +0000

    Changed word for placeholder

--- athletic_results/src/components/searchbar.js
+++ athletic_results/src/components/searchbar.js
@@ -13,12 +13,12 @@ export const SearchBar = () =>(
             text-gray-700
             bg-white bg-clip-padding
             border border-solid border-gray-300
-            rounded
+            rounded-full
             transition
             ease-in-out
             m-0
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            placeholder="search here"
+            placeholder="Search"
         />
     </form>
 );
