1d1227575ba5f300907ccbb22362699428e68451
Mei Hong <meilhong@students.k12k.com>
2022-12-02 16:48:22 +0000

    Changed search bar appearance

--- athletic_results/src/components/searchbar.js
+++ athletic_results/src/components/searchbar.js
@@ -1,8 +1,24 @@
 export const SearchBar = () =>(
    <form>
-        <p>SearchBar</p>
         <input 
-            
+            type="search"
+            class="
+            form-control
+            rounded-full
+            w-half
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            placeholder="search here"
         />
     </form>
 );
