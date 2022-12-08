9de27395659aa72d858d5282467b787b215ab7c8
Adyn Smith <adynasmith@students.k12k.com>
2022-12-08 17:43:59 +0000

    Change the title,add inputs & button,& add styling

--- admin/src/components/login.js
+++ admin/src/components/login.js
@@ -1,8 +1,12 @@
+import "../css/login.css"
 export function Login (){
     return (
         <div id="loginPage">
-            <h1>Login</h1>
-            <button>Login</button>
+            <h1 className="loginHeader">Login</h1>
+            <input className="usernameInput" placeholder="Username" />
+            <input className="passwordInput" placeholder="Password" />
+            <br/>
+            <button className="loginButton">Login</button>
         </div>
     )
 }
